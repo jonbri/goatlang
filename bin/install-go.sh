2a881e2df4551a885bac591220c0986734cf1af2
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-15 16:35:35 -0500

    chore: shebang

--- bin/install-go.sh
+++ bin/install-go.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz --output-dir /tmp
 sudo tar -C /usr/local -xvf /tmp/go1.16.7.linux-amd64.tar.gz
 export PATH=/usr/local/go/bin:$PATH
