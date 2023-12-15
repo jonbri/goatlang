2e94289e4e82c61204e5416a1ccee1b34c734e8b
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-15 10:06:29 -0500

    fix: refactor

---
+++ bin/install-go.sh
@@ -0,0 +1,4 @@
+curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz --output-dir /tmp
+sudo tar -C /usr/local -xvf /tmp/go1.16.7.linux-amd64.tar.gz
+export PATH=/usr/local/go/bin:$PATH
+
--- bin/version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-GOTAGGER="./bin/gotagger"
-GOATLANG_VERSION=`$GOTAGGER`
-
-go version
-$GOTAGGER --version
-echo "goatlang: $GOATLANG_VERSION"
-
