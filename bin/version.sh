7dac6c6a9d538ebf35e399cb0d717df2c3503742
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-15 09:49:26 -0500

    chore: clean up version

--- bin/version.sh
+++ bin/version.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 GOTAGGER="./bin/gotagger"
-
 GOATLANG_VERSION=`$GOTAGGER`
 
-echo
 go version
-echo
 $GOTAGGER --version
-echo
-echo "goatlang version: $GOATLANG_VERSION"
-echo
+echo "goatlang: $GOATLANG_VERSION"
 
