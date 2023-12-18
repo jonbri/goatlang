ae8ee20210908315f7e90d2e5f138d28c13aeb4e
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-18 13:19:33 -0500

    chore: cleanup

--- bin/release-commit.sh
+++ bin/release-commit.sh
@@ -7,14 +7,10 @@ new_version=`./bin/gotagger`
 echo "new_version: ${new_version}";
 
 npm version $new_version --commit-hooks false --git-tag-version false
-echo "after"
-cat package.json
 
 yarn changelog
-git add CHANGELOG.md
 
 npm version 0.0.0 --commit-hooks false --git-tag-version false
-echo "back to 0.0.0"
-cat package.json
 
-# git commit --allow-empty -m "release: $new_version"
+git add CHANGELOG.md
+git commit --allow-empty -m "release: $new_version"
