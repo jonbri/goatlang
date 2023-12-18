86e78eeee88bd99175a959446a33e2b292adeb07
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-18 16:00:16 -0500

    chore: generate-changelog

--- bin/generate-changelog.sh
+++ bin/generate-changelog.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-yarn
-yarn build
-
 new_version=`./bin/gotagger`
 echo "new_version: ${new_version}";
 
@@ -11,6 +8,3 @@ npm version $new_version --commit-hooks false --git-tag-version false
 yarn changelog
 
 npm version 0.0.0 --commit-hooks false --git-tag-version false
-
-git add CHANGELOG.md
-git commit --allow-empty -m "release: $new_version"
