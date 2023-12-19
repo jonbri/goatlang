11188579183170acb7a91c5d3ee2e654bd63e3c5
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-18 21:04:16 -0500

    chore: remove prelease.sh

--- bin/prerelease.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PREVIOUS_VERSION=`git describe --tags --abbrev=0`
-echo "PREVIOUS_VERSION: ${PREVIOUS_VERSION}"
-
-GOTAGGER_RESULT=`./bin/gotagger`
-echo "GOTAGGER_RESULT: $GOTAGGER_RESULT"
-./node_modules/.bin/semver $GOTAGGER_RESULT --increment prerelease --preid beta
