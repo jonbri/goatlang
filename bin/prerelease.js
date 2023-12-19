127953b97b68da6d872c3dc9fad30a6efa198ecc
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-19 10:41:17 -0500

    fix: more formatting

--- bin/prerelease.js
+++ bin/prerelease.js
@@ -5,11 +5,11 @@ async function determineNextPrereleaseVersion() {
   const gotaggerResult = execSync("./bin/gotagger").toString().trim();
   const tagList = execSync(`git tag --list ${gotaggerResult}-*`)
     .toString()
-    .trim();
+    .trim()
+    .split("\n");
   const versionExists = tagList.length > 0;
 
   const latestPrerelease = tagList
-    .split("\n")
     .map((tag) => tag.split(".").reverse()[0])
     .map((tag) => parseInt(tag))
     .sort((a, b) => b - a)[0];
