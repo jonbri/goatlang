e9d11565d3f2af81d9d1ef5e44111814e021bf2d
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-19 10:50:27 -0500

    fix: prefix

--- bin/prerelease.js
+++ bin/prerelease.js
@@ -1,26 +1,35 @@
 const { execSync } = require("child_process");
 const semverInc = require("semver/functions/inc");
 
+const debug = true;
+
 async function determineNextPrereleaseVersion() {
   const gotaggerResult = execSync("./bin/gotagger").toString().trim();
+  if (debug) console.log(`gotaggerResult: ${gotaggerResult}`);
+
   const tagList = execSync(`git tag --list ${gotaggerResult}-*`)
     .toString()
-    .trim()
-    .split("\n");
+    .trim();
+  if (debug) console.log(`tagList: ${tagList}`);
+
   const versionExists = tagList.length > 0;
+  if (versionExists) console.log(`version exists: ${versionExists}`);
 
   const latestPrerelease = tagList
+    .split("\n")
     .map((tag) => tag.split(".").reverse()[0])
     .map((tag) => parseInt(tag))
     .sort((a, b) => b - a)[0];
+  if (latestPrerelease) console.log(`latestPrerelease: ${latestPrerelease}`);
 
-  return versionExists
+  const nextVersion = versionExists
     ? semverInc(
         `${gotaggerResult}-beta.${latestPrerelease}`,
         "prerelease",
         "beta"
       )
     : `${gotaggerResult}-beta.0`;
+  return `v${nextVersion}`;
 }
 
 async function main() {
