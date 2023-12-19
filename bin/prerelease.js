6aff759a267828c71ef07a4b80e1fffa69335f80
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-19 11:00:08 -0500

    feat: comment

--- bin/prerelease.js
+++ bin/prerelease.js
@@ -3,6 +3,7 @@ const semverInc = require("semver/functions/inc");
 
 const debug = false;
 
+//
 async function determineNextPrereleaseVersion() {
   const gotaggerResult = execSync("./bin/gotagger").toString().trim();
   if (debug) console.log(`gotaggerResult: ${gotaggerResult}`);
