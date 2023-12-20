1425581f1c330014e76c8f1097fc0a249a3eb361
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-20 13:46:03 -0500

    chore: prerelease

--- bin/prerelease.js
+++ bin/prerelease.js
@@ -1,142 +1,79 @@
 const { execSync } = require("child_process");
 const semverInc = require("semver/functions/inc");
-// const { parseCommitsStream } = require("conventional-commits-parser");
-const conventionalCommitsParser = require("conventional-commits-parser");
-
-const releaseMode = process.argv[2] === "release" ? "release" : "prerelease";
-const debug = process.argv[3] === "true";
-const sortDesc = (a, b) => b - a;
-const shell = (cmd) => execSync(cmd).toString().trim();
-
-// async function determinePrereleaseVersion() {
-//   const gotaggerResult = shell("./bin/gotagger");
-//   if (debug) console.log(`gotaggerResult: ${gotaggerResult}`);
-//
-//   const matchingTags = shell(`git tag --list ${gotaggerResult}-*`);
-//   if (debug) console.log(`matchingTags: ${matchingTags}`);
-//
-//   const latestPrerelease = matchingTags
-//     .split("\n")
-//     .map((tag) => parseInt(tag.split(".").reverse()[0]))
-//     .sort(sortDesc)[0];
-//   if (debug) console.log(`latestPrerelease: ${latestPrerelease}`);
-//
-//   const incremented = semverInc(
-//     `${gotaggerResult}-beta.${latestPrerelease}`,
-//     "prerelease",
-//     "beta"
-//   );
-//   if (debug) console.log(`incremented: ${incremented}`);
-//
-//   const nextPrerelease = `v${incremented}`;
-//   const newPrerelease = `${gotaggerResult}-beta.0`;
-//   if (debug) console.log(`nextPrerelease: ${nextPrerelease}`);
-//   if (debug) console.log(`newPrerelease: ${newPrerelease}`);
-//
-//   const versionExists = matchingTags.length > 0;
-//   if (debug) console.log(`version exists: ${versionExists}`);
-//
-//   return versionExists ? nextPrerelease : newPrerelease;
-// }
-
 const ConventionalCommitsParser =
   "./node_modules/.bin/conventional-commits-parser";
 
-async function determineNextVersion() {
-  // loop through commits descending until a release tag is found
-  // if a prerelease tag is found, increment it
-  // if no prerelease tag is found, create one
+const shell = (cmd) => execSync(cmd).toString().trim();
+const TagRegex = /^v[0-9]\.[0-9]\.[0-9]*/;
 
-  const commits = shell("git rev-list HEAD").split("\n");
-  let releaseCommit;
-  let releaseTag;
+async function determineNextVersion(releaseMode, debug) {
   let highestBump = "patch"; // major, minor, patch
-
-  // let headVersion = "v0.9.0-beta.1"; // TODO:
   let headVersion;
-  for (const commit of commits) {
+  for (const commit of shell("git rev-list HEAD").split("\n")) {
     const shortCommit = commit.slice(0, 7);
     let releaseType;
-    try {
-      const version = shell(
-        `git describe ${commit} --tags --exact-match --match "v[0-9]\.[0-9]\.[0-9]*" 2>/dev/null`
-      );
-      // console.log(`${commit}: ${version}`);
 
-      // use conventional-commits-parser to determine the type of commit
-      // major, minor, or patch
+    const version = shell(`git tag --points-at ${commit}`)
+      .split("\n")
+      .filter((tag) => TagRegex.test(tag))[0];
 
-      if (version.includes("-")) {
-        // const prerelease = semverInc(version, "prerelease", "beta");
-        // console.log(`prerelease: ${prerelease}`);
-        releaseType = "prerelease";
-      } else {
-        // console.log(`release: ${version}`);
-        releaseType = "release";
-        releaseTag = version;
-      }
+    if (!version) {
+      if (debug) console.log(`${shortCommit} has no version tag`);
+      continue;
+    }
 
-      const parsedString = shell(
-        `git log --format=%B -n 1 ${commit} | ${ConventionalCommitsParser}`
-      );
-      const commitMetadata = JSON.parse(parsedString)[0];
-      // console.log(commitMetadata);
+    if (!headVersion) headVersion = version;
 
-      const commitType = commitMetadata.type;
-      const header = commitMetadata.header;
-      const footer = commitMetadata.footer;
-      // console.log(`commitType: ${commitType}`);
-
-      let bumpType = "patch";
-      if (commitType === "feat") {
-        bumpType = "minor";
-      } else if (header.includes("!") && footer.includes("BREAKING CHANGE")) {
-        // TODO: fix this logic
-        bumpType = "major";
-      }
-
-      if (bumpType === "major") {
-        highestBump = "major";
-      } else if (bumpType === "minor" && highestBump !== "major") {
-        highestBump = "minor";
-      }
-
-      if (debug)
-        console.log(
-          `${shortCommit} | ${version} | ${releaseType} | ${bumpType}`
-        );
-
-      if (!headVersion) headVersion = version;
-    } catch (e) {
-      if (debug) console.log(`${commit} has no tags`);
-    } // end of try
-
-    if (releaseType === "release") {
-      releaseCommit = commit;
+    if (version.includes("-")) {
+      releaseType = "prerelease";
+    } else {
+      releaseType = "release";
       break;
     }
-  } // end of for loop
+
+    const { type, header, footer } = JSON.parse(
+      shell(`git log --format=%B -n 1 ${commit} | ${ConventionalCommitsParser}`)
+    )[0];
+
+    let bumpType = "patch";
+    if (header.includes("!") && footer.includes("BREAKING CHANGE")) {
+      bumpType = "major";
+    } else if (type === "feat") {
+      bumpType = "minor";
+    }
+
+    if (bumpType === "major") {
+      highestBump = "major";
+    } else if (bumpType === "minor" && highestBump !== "major") {
+      highestBump = "minor";
+    }
+
+    if (debug) {
+      console.log(`${shortCommit} | ${version} | ${releaseType} | ${bumpType}`);
+    }
+  }
 
   const nextReleaseVersion = semverInc(headVersion, highestBump);
   const nextPrereleaseVersion = semverInc(headVersion, "prerelease", "beta");
 
-  const finalAnswer =
-    releaseMode === "release" ? nextReleaseVersion : nextPrereleaseVersion;
+  const nextVersion = `v${
+    releaseMode === "release" ? nextReleaseVersion : nextPrereleaseVersion
+  }`;
+
   if (debug) {
     console.log(`headVersion: ${headVersion}`);
     console.log(`highestBump: ${highestBump}`);
     console.log(`nextReleaseVersion: ${nextReleaseVersion}`);
     console.log(`nextPrereleaseVersion: ${nextPrereleaseVersion}`);
-    console.log(`releaseCommit: ${releaseCommit}`);
-    console.log(`releaseTag: ${releaseTag}`);
-    console.log(`releaseMode: ${releaseMode}, next version: ${finalAnswer}`);
   }
 
-  return `v${finalAnswer}`;
+  return nextVersion;
 }
 
 async function main() {
-  const nextVersion = await determineNextVersion();
+  const releaseMode = process.argv[2] === "release" ? "release" : "prerelease";
+  const debug = process.argv[3] === "true";
+  const nextVersion = await determineNextVersion(releaseMode, debug);
   console.log(nextVersion);
 }
 main();
