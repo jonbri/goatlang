06091fd82f599ca551ffcb120120d46285490065
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-20 20:04:33 -0500

    fix: syntax

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 # curl \
 #   -X POST \
@@ -11,6 +11,6 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/jonbri/goatlang/actions/workflows/prepare/dispatches \
+  https://api.github.com/repos/jonbri/goatlang/actions/workflows/prepare.yml/dispatches \
   -d '{"ref":"main"}'
 
