ef7b489cd79d4e721083c101033f4eed43a2f51a
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-20 19:59:48 -0500

    fix: permissions

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,7 +1,16 @@
 !#/bin/bash
 
-curl \
+# curl \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/jonbri/goatlang/actions/workflows/prepare/dispatches \
+#   -d '{"ref":"main"}'
+#
+curl -L \
   -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/jonbri/goatlang/actions/workflows/prepare/dispatches \
   -d '{"ref":"main"}'
+
