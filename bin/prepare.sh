5fccbdd13b4f4f7e894e0530a3b5eb317aef95c8
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-12-20 20:08:43 -0500

    fix: ok

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# curl \
-#   -X POST \
-#   -H "Accept: application/vnd.github.v3+json" \
-#   https://api.github.com/repos/jonbri/goatlang/actions/workflows/prepare/dispatches \
-#   -d '{"ref":"main"}'
-#
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
