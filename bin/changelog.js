12ccdad5a4a10bf81742dc2f73c87aa4262af305
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-01-09 10:19:13 -0500

    fix: skip unstable

--- bin/changelog.js
+++ bin/changelog.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
-const ConventionalChangelog = require("conventional-changelog");
+const conventionalChangelog = require("conventional-changelog");
 
 const shell = (cmd) => execSync(cmd).toString().trim();
 const tag = process.argv[2];
 
 // https://stackoverflow.com/a/49428486/2295034
-function streamToString(stream) {
+const streamToString = (stream) => {
   const chunks = [];
   return new Promise((resolve, reject) => {
     stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
     stream.on("error", (err) => reject(err));
     stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
   });
-}
+};
 
 async function main() {
   const version = tag
@@ -21,14 +21,15 @@ async function main() {
     : shell('node ./bin/version --value="baseVersion"').replace(/^v/, "");
   console.log(`version: ${version}`);
 
-  const stream = ConventionalChangelog({
+  const stream = conventionalChangelog({
     preset: "angular",
     releaseCount: 0,
+    skipUnstable: true,
     pkg: {
-      transform(pkg) {
-        pkg.version = version;
-        return pkg;
-      },
+      transform: (pkg) => ({
+        ...pkg,
+        version,
+      }),
     },
   });
 
